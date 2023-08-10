791a28538cb67521a63406cc4eea34120aaa5e14
Manuel Duarte  <carlosd99m@gmail.com>
2023-08-10 00:43:51 -0600

    queryset and urls

---
+++ django-react/tasks/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task 
+        fields = '__all__' # This will serialize all the fields in the model
