84ffa916d6c759be785b7a9938433471f08c9467
Manuel Duarte  <carlosd99m@gmail.com>
2023-08-10 00:53:21 -0600

    docs

--- django-react/tasks/urls.py
+++ django-react/tasks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 from . import views
 
 router = routers.DefaultRouter()
@@ -7,4 +8,5 @@ router.register(r'tasks', views.TaskView, 'tasks')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
+    path('docs/', include_docs_urls(title='Task API', description='RESTful API for managing tasks'))
 ]
