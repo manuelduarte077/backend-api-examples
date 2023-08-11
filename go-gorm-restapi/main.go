a63460f9ebffb6ed686a463cd31b0e1d3b56939b
Manuel Duarte  <carlosd99m@gmail.com>
2023-08-11 15:12:05 -0600

    groups routes

--- go-gorm-restapi/main.go
+++ go-gorm-restapi/main.go
@@ -29,6 +29,12 @@ func main() {
 	r.HandleFunc("/users", routes.CreateUserHandler).Methods("POST")
 	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
 
+	/// Group router
+	r.HandleFunc("/groups", routes.GetGroupsHandler).Methods("GET")
+	r.HandleFunc("/groups/{id}", routes.GetGroupHandler).Methods("GET")
+	r.HandleFunc("/groups", routes.CreateGroupHandler).Methods("POST")
+	r.HandleFunc("/groups/{id}", routes.DeleteGroupHandler).Methods("DELETE")
+
 	http.ListenAndServe(":3000", r)
 
 }
--- go-gorm-restapi/routes/groups.routes.go
+++ go-gorm-restapi/routes/groups.routes.go
@@ -1,1 +1,62 @@
 package routes
+
+import (
+	"encoding/json"
+	"github/manuelduarte077/go-gorm-restapi/db"
+	"github/manuelduarte077/go-gorm-restapi/models"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func GetGroupsHandler(w http.ResponseWriter, r *http.Request) {
+	var groups []models.Group // Slice de grupos
+	db.DB.Find(&groups)       // SELECT * FROM groups
+
+	json.NewEncoder(w).Encode(groups) // Convertimos el slice a JSON
+}
+
+func GetGroupHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)             // Obtenemos los parámetros de la URL
+	var group models.Group            // Creamos una instancia de Group
+	db.DB.First(&group, params["id"]) // SELECT * FROM groups WHERE id = params["id"]
+
+	if group.ID == 0 {
+		w.WriteHeader(http.StatusNotFound) // Si no existe el grupo, devolvemos un 404
+		w.Write([]byte("Group not found")) // Y un mensaje de error
+		return                             // Cortamos la ejecución
+	}
+
+	db.DB.Model(&group).Association("Tasks").Find(&group.Tasks) // SELECT * FROM tasks WHERE group_id = group.ID
+	json.NewEncoder(w).Encode(group)                            // Si no hay error, devolvemos el grupo
+}
+
+func CreateGroupHandler(w http.ResponseWriter, r *http.Request) {
+	var group models.Group                 // Creamos una instancia de Group
+	json.NewDecoder(r.Body).Decode(&group) // Convertimos el JSON a un objeto Group
+	createdGroup := db.DB.Create(&group)   // Insertamos el grupo en la base de datos
+	err := createdGroup.Error              // Obtenemos el error de la operación
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // Si hay un error, devolvemos un 400
+		w.Write([]byte(err.Error()))         // Y el mensaje de error
+	}
+
+	json.NewEncoder(w).Encode(group) // Si no hay error, devolvemos el grupo creado
+
+}
+
+func DeleteGroupHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)             // Obtenemos los parámetros de la URL
+	var group models.Group            // Creamos una instancia de Group
+	db.DB.First(&group, params["id"]) // SELECT * FROM groups WHERE id = params["id"]
+
+	if group.ID == 0 {
+		w.WriteHeader(http.StatusNotFound) // Si no existe el grupo, devolvemos un 404
+		w.Write([]byte("Group not found")) // Y un mensaje de error
+		return                             // Cortamos la ejecución
+	}
+
+	db.DB.Unscoped().Delete(&group) // DELETE FROM groups WHERE id = params["id"]
+	w.WriteHeader(http.StatusOK)    // Si no hay error, devolvemos un 200
+}
