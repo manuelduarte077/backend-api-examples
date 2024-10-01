a37057889eb5a5718d203d9f3cf6ca0edeb5eaf8
Manuel Duarte <carlosd99m@gmail.com>
2024-10-01 01:25:15 -0600

    feat: Add initial files for Shop API setup

---
+++ CrudApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using CrudApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudApp.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+  }
+}
